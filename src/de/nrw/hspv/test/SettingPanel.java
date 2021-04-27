4ffc71fe3eed3b4ea1f2059d8d8a660768432ea8
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-04-27 17:51:01 +0200

    SettingPanel hinzugefügt
    Panel, welches die Oberfläche beschreibt, wenn man auf Eiunstellungen
    klickt.

---
+++ src/de/nrw/hspv/test/SettingPanel.java
@@ -0,0 +1,17 @@
+package de.nrw.hspv.test;
+import java.awt.Color;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+public class SettingPanel extends JPanel {
+
+	SettingPanel(){
+	
+		this.setBackground(Color.DARK_GRAY.brighter());
+	}
+	
+}
