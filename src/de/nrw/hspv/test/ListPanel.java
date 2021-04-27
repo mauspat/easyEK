84cdf5e9c31c6d16272056f63da3be1b30d33ddb
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-04-27 17:47:48 +0200

    ListPanel hinzugefügt: Klasse beschreibt die UI der Anzeige aller Listen

---
+++ src/de/nrw/hspv/test/ListPanel.java
@@ -0,0 +1,15 @@
+package de.nrw.hspv.test;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class ListPanel extends JPanel{
+	
+	ListPanel(){
+		this.setBackground(Color.DARK_GRAY);
+		
+		
+	}
+
+}
