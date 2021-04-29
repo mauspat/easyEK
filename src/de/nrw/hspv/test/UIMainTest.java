03ec76374bc39cac965f316c4defa41ee5349838
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-04-30 00:12:10 +0200

    Demo ChangeToolbarButtons - zu Testzwecken

--- src/de/nrw/hspv/test/UIMainTest.java
+++ src/de/nrw/hspv/test/UIMainTest.java
@@ -4,7 +4,16 @@ public class UIMainTest {
 
 	public static void main(String[] args) {
 		Ui startingWindow = new Ui();
-
+		int i=startingWindow.getToolbar().getComponentCount();
+		System.out.println(i);
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		startingWindow.ChangeToolbarButton(new EKButton("new"));
 	}
 
 }
