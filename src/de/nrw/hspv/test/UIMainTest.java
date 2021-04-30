2bb7607e6408a8f61b192f9968c9a72cf0aaeeb7
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-04-30 14:32:40 +0200

    Testzwecke - minimale Änderungen

--- src/de/nrw/hspv/test/UIMainTest.java
+++ src/de/nrw/hspv/test/UIMainTest.java
@@ -3,17 +3,19 @@ package de.nrw.hspv.test;
 public class UIMainTest {
 
 	public static void main(String[] args) {
-		Ui startingWindow = new Ui();
-		int i=startingWindow.getToolbar().getComponentCount();
-		System.out.println(i);
+		new Ui();
+//		int i=startingWindow.getToolbar().getComponentCount();
+//		System.out.println(i);
+//		
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		startingWindow.ChangeToolbarButton(new EKButton("new"));
 		
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		startingWindow.ChangeToolbarButton(new EKButton("new"));
+		
 	}
 
 }
