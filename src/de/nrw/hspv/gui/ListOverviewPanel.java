09f7542000fbc89db0059edca799a9649576649b
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-05-10 14:10:59 +0200

    zwischenspeichern - ListOverview Panel mit Backend verküpfen

--- src/de/nrw/hspv/gui/ListOverviewPanel.java
+++ src/de/nrw/hspv/gui/ListOverviewPanel.java
@@ -7,54 +7,39 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import de.nrw.hspv.backend.ShoppingList;
+
 
 
 public class ListOverviewPanel extends JPanel{
 	
-	
-	//TODO Actual list muss mit geklicktem Button verbunden werden, damit die richtige Liste an das ListPanel übergeben wird.
-	private ArrayList<String> actualList01 = new ArrayList<String>();
-	private ArrayList<String> actualList02= new ArrayList<String>();
+	ArrayList<ShoppingList> allShoppingLists = ShoppingList.getAllShoppingLists();
 	
 	ListOverviewPanel(){
 		
-		//TODO Main Panel wieder auf private setzten
-		JButton button = new JButton();
 		
-		actualList01.add("Stuff");
-		button.setText("Liste 1");
-		button.addActionListener(new ActionListener() {
+		this.setBackground(UI.getBgColor().brighter());
+		
+		for (ShoppingList sl: allShoppingLists) {
+			EKButton button = new EKButton();
+			button.setText(sl.getName());
+			button.addActionListener(new ActionListener() {
 
-			@Override
-			//Der Button erstellt eine neue Ebene auf dem CardLayout der Klasse Ui. Anschließend wird das erstellte Panel aufgerufen und damit eingeblendet.
-			public void actionPerformed(ActionEvent e) {
-				UI.getMainPanel().add(new ListPanel(actualList01), "actualList");
-				UI.getCl().show(UI.getMainPanel(), "actualList");
-			
-			}
-			
-		});
-		this.add(button);
-		
-		
-		
-		JButton button2 = new JButton();
-		actualList02.add("Mango Mango");
-		actualList02.add("Banananaaaaana nana");
-		actualList02.add("MilleniumFalke");
-		actualList02.add("StormTrooper");
-		
-		button2.setText("Liste 2");
-		button2.addActionListener(new ActionListener() {
+				@Override
+				//Der Button erstellt eine neue Ebene auf dem CardLayout der Klasse Ui. Anschließend wird das erstellte Panel aufgerufen und damit eingeblendet.
+				public void actionPerformed(ActionEvent e) {
+					UI.getMainPanel().add(new ListPanel(sl), "actualList");
+					UI.getCl().show(UI.getMainPanel(), "actualList");
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-			UI.getMainPanel().add(new ListPanel(actualList02), "actualList");
-			UI.getCl().show(UI.getMainPanel(), "actualList");
-			}
-			
-		});
-		this.add(button2);
+				}
+
+			});
+			this.add(button);
+		}
+		
+		
+		
+		
 	}
 	
 }
