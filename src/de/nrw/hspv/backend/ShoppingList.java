f50d285f80cd06cfb01fa3fa6ddf8b6554aa7b49
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-05-10 14:14:17 +0200

    Sort-Algorythmus in ShoppingList hinzugefügt (aus Main heraus)

--- src/de/nrw/hspv/backend/ShoppingList.java
+++ src/de/nrw/hspv/backend/ShoppingList.java
@@ -1,6 +1,7 @@
 package de.nrw.hspv.backend;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,12 +14,21 @@ public class ShoppingList implements Serializable {
 	 * 
 	 */
 
-	public static ArrayList<ShoppingList> savedLists = new ArrayList<ShoppingList>();
+	private static ArrayList<ShoppingList> savedLists = new ArrayList<ShoppingList>();
 	private static final long serialVersionUID = 1L;
 	private int counter = 1;
 	private String name;
 	private int id;
+	private Supermarket supermarket;
 	private ArrayList<Item> shoppingList = new ArrayList<Item>();
+	
+	public ShoppingList(String name, Supermarket supermarket) {
+		this.name = name;
+		this.id = counter;
+		this.supermarket=supermarket;
+		savedLists.add(this);
+		counter += 1;
+	}
 	
 	public ShoppingList(String name) {
 		this.name = name;
@@ -26,6 +36,103 @@ public class ShoppingList implements Serializable {
 		savedLists.add(this);
 		counter += 1;
 	}
+	
+	
+	
+	
+	
+	
+	//__-------------------------------------------------
+	/** 
+	 * Die contains-Methode erhält ein int-Array sowie einen int-Wert als Übergabeparameter. Sie überprüft dann,
+	 * ob der int-Wert im Array enthalten ist. In diesem Fall liefert die Methode den Wert true zurück,
+	 * ansonsten den Wert false.
+	 */
+	private  boolean containsCategory(final int[] supermarket, final int categoryID) {
+		// Die Lambda-Expression "c -> c == categoryID" nutzt das funktionale Interface
+		// "IntPredicate" und überprüft, ob c dem Wert der übergebenen Variablen "categoryID"
+		// entspricht. Ist dies der Fall, ist der Rückgabewert true, ansonsten false.
+		return Arrays.stream(supermarket).anyMatch(c -> c == categoryID);
+	}
+	
+	// Methode zum Sortieren der Einkaufsliste nach dem jeweiligen Supermarkt
+	/**
+	 * listSort sortiert eine übergebene Liste (ArrayList vom Typ Item) anhand eines ebenfalls übergebenenen
+	 * int-Arrays. Jeder Eintrag aus der Liste wird mit dem aktuellen int-Wert des Arrays abgeglichen;
+	 * stimmen die Werte überein, wird das Item auf die sortierte Einkaufsliste gesetzt.
+	 * Die Werte des int-Arrays entsprechend dabei der Sortier-Reihenfolge des Supermarktes. Items,
+	 * die in einem Supermarkt nicht gefunden werden können, werden unten an die Liste angehängt.
+	 * @param list
+	 * @param supermarket
+	 * @return ArrayList<Item> sortedList
+	 */
+	public  ArrayList<Item> sortList() {
+		
+		int[] supermarketGrid = this.supermarket.getSupermarketGrid();
+		// Die Liste categorized erhält vorab alle Items, deren Kategorien im gewählten Supermarkt gefunden werden.
+		ArrayList<Item> categorized = new ArrayList<Item>();
+		
+		// Auf die bucketList werden die Items gepackt, deren Kategorien im gewählten Supermarkt nicht vorkommen.
+		// Die bucketList ist nur temporär, alle Items auf der bucketList werden zum Schluss an die
+		// sortedList angehängt, damit diese Items ganz unten auf der Liste stehen.
+		ArrayList<Item> bucketList = new ArrayList<Item>();
+		
+		// in sortedList werden die sortierten Einträge gespeichert
+		ArrayList<Item> sortedList = new ArrayList<Item>();
+		
+		// Hier wird vorsortiert - Wenn die Kategorie-ID des Produktes als Kategorie im
+		// Supermarkt vorkommt, wird das Item auf die categorized-Liste gesetzt,
+		// ansonsten auf die bucketList.
+		Iterator<Item> rawItr = this.shoppingList.iterator();
+		while(rawItr.hasNext()) {
+			Item i = rawItr.next();
+			if(containsCategory(supermarketGrid, i.getCategory().getCategoryID())) {
+				categorized.add(i);
+			} else {
+				bucketList.add(i);
+			}
+		}
+		
+		// Die categorized-Liste wird durchlaufen; jeder Eintrag wird mit dem Supermarkt-Array
+		// abgeglichen; wenn die ID des aktuellen Items dem aktuellen Supermarkt-Array-Wert gleicht,
+		// wird das Item auf die Liste sortedList gesetzt.
+		for(int i = 0; i < supermarketGrid.length; i++) {
+			Iterator<Item> sortItr = categorized.iterator();
+			while(sortItr.hasNext()) {
+				Item sorter = sortItr.next();
+				if(sorter.getCategory().getCategoryID() == supermarketGrid[i]) {
+					sortedList.add(sorter);
+				}
+			}
+		}
+		
+		// Alle Items auf der bucketList werden jetzt an die sortedList angehängt
+		Iterator<Item> bucket = bucketList.iterator();
+		while(bucket.hasNext()) {
+			Item temp = bucket.next();
+			sortedList.add(temp);
+		}
+		
+		// Die sortierte Liste wird als Rückgabewert der Funktion zurückgegeben und die Liste wird auf die umsortierte Liste gesetzt.
+		this.shoppingList = sortedList;
+		return sortedList;
+	}
+	
+	//__-------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/**
 	 * Die Methode addToList fügt der ArrayList shoppingList ein Item hinzu.
@@ -109,4 +216,17 @@ public class ShoppingList implements Serializable {
 	public void printList() {
 		shoppingList.stream().forEach(e -> System.out.println(e));
 	}
+	public static ArrayList<ShoppingList> getAllShoppingLists() {
+		return savedLists;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<Item> getShoppingList(){
+		return shoppingList;
+	}
+	
+	
 }
