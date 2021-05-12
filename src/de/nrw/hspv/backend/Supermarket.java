8eb1e812f00ceb10ce6b47e881ee7d2de8dc5d88
Lukas Wisniewski <lu.wisniewski@gmx.de>
2021-05-12 02:07:32 +0200

    Addressennummer gelöscht und Konstruktor angepasst

--- src/de/nrw/hspv/backend/Supermarket.java
+++ src/de/nrw/hspv/backend/Supermarket.java
@@ -1,117 +1,137 @@
 package de.nrw.hspv.backend;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Supermarket {
-	
+
 	private static ArrayList<Supermarket> supermarketList = new ArrayList<Supermarket>();
-	
+
 	private static int counter = 0;
 	private int id;
 	private int postalCode;
 	private String name;
 	private String street;
-	private int addrNumber;
+
 	private String city;
 	private int[] grid;
-	
-	Supermarket(){
-		
+
+	public Supermarket() {
+
 	}
-	
+
 	// --------- KONSTRUKTOREN ---------
-	Supermarket(String name, int[] grid) {
+	public Supermarket(String name, int[] grid) {
 		this.name = name;
 		this.grid = grid;
 		this.id = counter + 1;
 		counter += 1;
-		
-		//--------- Standardwerte ---------
+
+		// --------- Standardwerte ---------
 		street = "N/A"; // N/A = Not Available
 		city = "N/A";
-		addrNumber = 9999;
+
 		postalCode = 9999;
-		//---------------------------------
-		
+		// ---------------------------------
+
 		supermarketList.add(this);
 	}
-	
-	Supermarket(String name, String city, int[] grid) {
+
+	public Supermarket(String name, String city, int[] grid) {
 		this.name = name;
 		this.city = city;
 		this.grid = grid;
 		this.id = counter + 1;
 		counter += 1;
-		
-		//--------- Standardwerte ---------
+
+		// --------- Standardwerte ---------
 		street = "N/A";
-		addrNumber = 9999;
+
 		postalCode = 9999;
-		//---------------------------------
-		
+		// ---------------------------------
+
 		supermarketList.add(this);
 	}
-	
-	Supermarket(String name, String city, String street, int addrNumber, int postalCode, int[] grid) {
+
+	public Supermarket(String name, String city, String street,  int postalCode, int[] grid) {
 		this.name = name;
 		this.city = city;
 		this.street = street;
-		this.addrNumber = addrNumber;
+
 		this.postalCode = postalCode;
 		this.grid = grid;
 		this.id = counter + 1;
 		counter += 1;
-		
+
 		supermarketList.add(this);
 	}
-	
+
+	/**
+	 * Die methode vergleicht alle in der übegebenen Supermarkts Liste enthaltenen Elemente mit denen, welche in der supermarketList stehen.
+	 * Wird ein identisches Element gefunden, wird dieses aus der supermarketList gelöscht.
+	 * 
+	 * @param supermarkets
+	 */
+	public static void deleteSupermarket(List<Supermarket> supermarkets) {
+
+		Iterator<Supermarket> selectedMarkets = supermarkets.iterator();
+		while (selectedMarkets.hasNext()) {
+			Supermarket deleteThisMarket = selectedMarkets.next();
+
+			Iterator<Supermarket> allMarkets = supermarketList.iterator();
+			while(allMarkets.hasNext()) {
+				if(deleteThisMarket.equals(allMarkets.next())) {
+					allMarkets.remove();
+
+					
+				}
+			}
+		}
+		
+		
+		
+	}
+
 	@Override
 	public String toString() {
 		return this.name;
 	}
-	
 
-	
-	//--------- SETTERS ---------
+	// --------- SETTERS ---------
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setPostalCode(int postalCode) {
 		this.postalCode = postalCode;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
-	public void setAdressNumber(int addrNumber) {
-		this.addrNumber = addrNumber;
-	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public static void setSupermarketList(ArrayList<Supermarket> supermarketList) {
 		Supermarket.supermarketList = supermarketList;
 	}
 
-	//--------- GETTERS ---------
+	// --------- GETTERS ---------
 	public Supermarket getSupermarket() {
 		return this;
 	}
-	
+
 	public String getSupermarketName() {
 		return name;
 	}
-	
+
 	public int[] getSupermarketGrid() {
 		return grid;
 	}
-	
-	public String getAdress() {
-		
-		return ((street+ " " + addrNumber)+", " +(postalCode+" "+ city));
-		
-	}
-	
-	
+
 	public static ArrayList<Supermarket> getSupermarketList() {
 		return supermarketList;
 	}
